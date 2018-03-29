7c30f749877ff6f01275296790e91eb69a29c2a8
step-han <step_han@gmx.de>
2018-03-29 11:38:59 +0200

    initial commit

--- get_host_hardware.ps1
+++ get_host_hardware.ps1
@@ -7,7 +7,7 @@ $vCenter = @("vCenter_1","vCenter_2")
 foreach ($vCntr in $vCenter) {
 	Connect-VIServer -Server $vCntr
 
-	Get-VMHost | Get-View | Select @{N="Physical Machine Name";E={$_.Name}}, @{N=“Server Make and Model“;E={$_.Hardware.SystemInfo.Vendor+ “ “ + $_.Hardware.SystemInfo.Model}}, @{N=“Processor Model and Speed (Mhz)“;E={$_.Hardware.CpuPkg[0].Description}}, @{N=“# Physical CPUs“;E={$_.Hardware.CpuInfo.NumCpuPackages}}, @{N="# Cores per Physical CPU";E={$_.Hardware.CpuInfo.NumCpuCores}} | Export-Csv -Append -Path C:\Users\admleddin\Desktop\hostinfo.csv
+	Get-VMHost | Get-View | Select @{N="Physical Machine Name";E={$_.Name}}, @{N=“Server Make and Model“;E={$_.Hardware.SystemInfo.Vendor+ “ “ + $_.Hardware.SystemInfo.Model}}, @{N=“Processor Model and Speed (Mhz)“;E={$_.Hardware.CpuPkg[0].Description}}, @{N=“# Physical CPUs“;E={$_.Hardware.CpuInfo.NumCpuPackages}}, @{N="# Cores per Physical CPU";E={$_.Hardware.CpuInfo.NumCpuCores}} | Export-Csv -Append -Path C:\Users\USERNAME\Desktop\hostinfo.csv
 
 	Disconnect-VIServer -Confirm:$false
 }
---
+++ get_host_information.ps1
@@ -0,0 +1,37 @@
+if ( !(Get-Module -Name VMware.VimAutomation.Core) ) {
+    ."C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
+ }
+ 
+ $vCenter = "vCenter"
+ $col = @()
+ 
+ Connect-VIServer -Server $vCenter
+ 
+ foreach ($cluster in Get-Cluster) {
+     foreach ($esx in ($cluster | Get-VMHost)) {
+         $item = New-Object PSObject
+ 
+         $hostView = $esx | Get-View
+         $item | Add-Member -MemberType NoteProperty -Name "Hostname" -Value $esx.Name
+         $item | Add-Member -MemberType NoteProperty -Name "ProcessorType" -Value $hostView.Hardware.CpuPkg[0].Description
+         $item | Add-Member -MemberType NoteProperty -Name "Sockets" -Value $hostView.hardware.cpuinfo.numCpuPackages #$hostView.Hardware.CpuPkg[0].Description
+         $item | Add-Member -MemberType NoteProperty -Name "CoresPerSocket" -Value $($hostView.Hardware.CpuInfo.NumCpuCores/2)
+         $item | Add-Member -MemberType NoteProperty -Name "Memory" -Value $([math]::Round($esx.MemoryTotalGB)) #$hostView.hardware.memorysize/1024Mb
+         $item | Add-Member -MemberType NoteProperty -Name "Model" -Value $esx.Model
+ 
+         foreach ($hba in Get-VMHostHba -VMHost $esx) {
+             $hbaDevice = $hba | select Device
+             $hbaModel = $hba | select Model
+             $item | Add-Member -MemberType NoteProperty -Name $hbaDevice.Device.Normalize() -Value $hbaModel.Model.Normalize()
+         }
+ 
+         foreach ($nic in $($(Get-EsxCli -VMHost $esx -V2).network.nic.list.Invoke())) {
+             $item | Add-Member -MemberType NoteProperty -Name $nic.Name -Value $nic.Description
+         }
+         $col += $item
+     }
+ }
+ $col #| Out-GridView
+ $col = @()
+ 
+ Disconnect-VIServer -Confirm:$false
